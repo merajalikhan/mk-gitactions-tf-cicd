8c604562a331d7783de6cb5298b82955a88235fb
merajalikhan <merajalikhan@hotmail.com>
2023-05-17 18:16:46 +0100

    testete

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -22,8 +22,8 @@ terraform {
 }
 
 provider "databricks" {
-  host = var.databricks_host #"https://adb-1710922579126448.8.azuredatabricks.net/" 
-  token = var.databricks_token # "dapid24a84b27054a5fccabc962278928be0-3" #
+  #host = var.databricks_host #"https://adb-1710922579126448.8.azuredatabricks.net/" 
+  #token = var.databricks_token # "dapid24a84b27054a5fccabc962278928be0-3" #
 }
 
 //data "databricks_current_user" "logged_in_user" {}
