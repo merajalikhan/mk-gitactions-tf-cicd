790a1bcd742c97ec4810c1db321e0fad200f55c1
merajalikhan <merajalikhan>
2023-05-18 15:32:19 +0000

    checked in from dbricks

--- Terraform/Notebooks/MK-TEST-20230518.py
+++ Terraform/Notebooks/MK-TEST-20230518.py
@@ -1,2 +1,16 @@
 # Databricks notebook source
 print ('mk test notebook!!')
+
+# COMMAND ----------
+
+credit_data = spark.read.format("csv") \
+                        .schema(custom_schema)\
+                        .option("header", "True")\
+                        .option("sep", ",") \
+                        .load("dbfs:/FileStore/shared_uploads/credit_train.csv")
+
+# COMMAND ----------
+
+credit_data = credit_data \
+           .write.mode('append')\
+           .saveAsTable("creditdatawarehouse.credit_data_monthly_debt")
