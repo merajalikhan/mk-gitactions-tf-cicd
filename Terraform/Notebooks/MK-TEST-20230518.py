c5d3bb8fc55a2f8689fd58b154b1b43246eea4e8
merajalikhan <merajalikhan>
2023-05-18 15:51:45 +0000

    checked in from dbricks

--- Terraform/Notebooks/MK-TEST-20230518.py
+++ Terraform/Notebooks/MK-TEST-20230518.py
@@ -3,14 +3,18 @@ print ('mk test notebook!!')
 
 # COMMAND ----------
 
-credit_data = spark.read.format("csv") \
-                        .schema(custom_schema)\
-                        .option("header", "True")\
-                        .option("sep", ",") \
-                        .load("dbfs:/FileStore/shared_uploads/credit_train.csv")
+def reverse(s):
+    return s[::-1]
+
+import unittest
+
+class TestHelpers(unittest.TestCase):
+    def test_reverse(self):
+        self.assertEqual(reverse('abc'), 'scba')
+
+r = unittest.main(argv=[''], verbosity=2, exit=False)
+assert r.result.wasSuccessful(), 'Test failed; see logs above'
 
 # COMMAND ----------
 
-credit_data = credit_data \
-           .write.mode('append')\
-           .saveAsTable("creditdatawarehouse.credit_data_monthly_debt")
+
