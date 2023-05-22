c0b914af14427bae2639ac663b69c46d4af57131
merajalikhan <merajalikhan@hotmail.com>
2023-05-22 11:57:16 +0100

    add new cluster

--- Terraform/cluster.tf
+++ Terraform/cluster.tf
@@ -11,6 +11,16 @@ resource "databricks_cluster" "shared_autoscaling" {
   
 }
 
+resource "databricks_cluster" "shared_autoscaling1" {
+  cluster_name            = "KG Cluster"
+  spark_version           = data.databricks_spark_version.latest_lts.id #"12.2 LTS (includes Apache Spark 3.3.2, Scala 2.12)"
+  node_type_id            = "Standard_DS3_v2"
+  autotermination_minutes = 15   
+  num_workers = 1
+ 
+  
+}
+
 
 output "cluster_id" {
  value = databricks_cluster.shared_autoscaling.id
