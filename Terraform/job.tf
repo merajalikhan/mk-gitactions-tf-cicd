603cc84095941fdbcd368388db92518b8d2faae2
merajalikhan <merajalikhan@hotmail.com>
2023-05-25 13:30:13 +0100

    added wf to destroy tf

--- Terraform/job.tf
+++ Terraform/job.tf
@@ -11,11 +11,29 @@ resource "databricks_job" "diamonds-data-job" {
     on_start = [var.notification_email]
   }
   schedule {
-    quartz_cron_expression =  "0 40 18 * * ?" 
+    quartz_cron_expression =  "0 25 11 * * ?" 
     timezone_id = "Europe/London"
   }
 }
 
+
+resource "databricks_job" "credit-data-job" {
+  name = "credit-data-job"
+  existing_cluster_id =  var.existing_cluster_id  #databricks_cluster.this.cluster_id
+  #existing_cluster_id =  data.databricks_cluster.my_cluster.id  
+  notebook_task {
+    notebook_path = data.databricks_notebook.all_notebooks_data["mk-credit_data.py"].id#  data.databricks_notebook.diamonds.id
+  }
+  email_notifications {
+    on_success = [var.notification_email]
+    on_failure = [var.notification_email]
+    on_start = [var.notification_email]
+  }
+  schedule {
+    quartz_cron_expression =  "0 25 12 * * ?" 
+    timezone_id = "Europe/London"
+  }
+}
 output "job_url" {
   value = databricks_job.diamonds-data-job.url
 }
