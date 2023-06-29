cfa730b4fb5f6f3eeaf09dd9eebcac548b4c7342
Meraj Khan <merajakhan@hotmail.com>
2023-06-29 15:49:11 +0100

    added back the test stage

--- Terraform/Notebooks/UnitTests/mk-all-unit-tests.py
+++ Terraform/Notebooks/UnitTests/mk-all-unit-tests.py
@@ -9,26 +9,37 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 def tableExists(tableName, dbName):  
-  returnValue=  spark.catalog.tableExists(f"{dbName}.{tableName}")
-  if returnValue == True:
-    print(f"Table '{tableName}' exist in database '{dbName}'.") 
-  else:
-    print(f"Table '{tableName}' does not exist in database '{dbName}'.")   
-  return  returnValue
+  try:
+    returnValue=  spark.catalog.tableExists(f"{dbName}.{tableName}")
+    if returnValue == True:
+      print(f"Table '{tableName}' exist in database '{dbName}'.") 
+    else:
+      print(f"Table '{tableName}' does not exist in database '{dbName}'.")   
+    return  returnValue
+  except Exception e:
+    exit('ERROR: Failed with error  :  {} '.format(e))
+
 
 def columnExists(dataFrame, tableName, columnName):
-  if columnName in dataFrame.columns: 
-    print(f"Column '{columnName}' exist in table '{tableName}' .") 
-    return True
-  else:
-    print(f"Column '{columnName}' does not exist in table '{tableName}'.")   
-    return False
+  returnValue = False
+  try:
+    if columnName in dataFrame.columns: 
+      print(f"Column '{columnName}' exist in table '{tableName}' .") 
+      return True
+    else:
+      print(f"Column '{columnName}' does not exist in table '{tableName}'.")   
+      return False
+  except Exception e:
+    exit('ERROR: Failed with error  :  {} '.format(e))
 
 def numRowsInColumnForValue(dataFrame, tableName, columnName, columnValue):
-  df = dataFrame.filter(col(columnName) == columnValue)
-  numRows = df.count()
-  print(f"There are {numRows} rows in '{tableName}' where '{columnName}' equals '{columnValue}'.")
-  return numRows
+  try:
+    df = dataFrame.filter(col(columnName) == columnValue)
+    numRows = df.count()
+    print(f"There are {numRows} rows in '{tableName}' where '{columnName}' equals '{columnValue}'.")
+    return numRows
+  except Exception e:
+    exit('ERROR: Failed with error  :  {} '.format(e))
 
 
 # COMMAND ----------
